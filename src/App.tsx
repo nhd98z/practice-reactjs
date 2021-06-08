875233a1d4c90e79b4a6568d9c487da7f4d7dfd0
Nguyễn Hữu Dũng <nguyenhuudungz@NHDz-C07F6.local>
2021-06-08 09:52:10 +0700

    Keyboard Navigation with <div> tag with tabindex=0

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import KeyboardNavigation from "./pages/KeyboardNavigation";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/keyboard-navigation">Keyboard Navigation</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route strict path="/keyboard-navigation">
+            <KeyboardNavigation />
+          </Route>
+          <Route strict path="/">
+            <Home />
+          </Route>
+          <Route>
+            <Redirect to={{ pathname: "/" }} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/KeyboardNavigation/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const StyledDiv = styled.div`
+  :focus {
+    background: red;
+  }
+`;
+
+export default function KeyboardNavigation() {
+  return (
+    <>
+      <button>This is a button</button>
+      <StyledDiv>This is a div</StyledDiv>
+      <StyledDiv
+        tabIndex={0}
+        onClick={() => {
+          console.log("clicked");
+        }}
+      >
+        This is a div (has onClick event) and tabindex="0"
+      </StyledDiv>
+      <button>This is a button</button>
+      <button>This is a button</button>
+      <StyledDiv
+        tabIndex={0}
+        onClick={() => {
+          console.log("clicked");
+        }}
+      >
+        This is a div (has onClick event) and tabindex="0"
+      </StyledDiv>
+      <button>This is a button</button>
+    </>
+  );
+}
