4c8e9bbc41ede8d2ecdf6690095f1ff30a30dfc6
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2021-06-14 10:28:12 +0700

    build: remove git cz

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">Homez</Link>
           </li>
           <li>
             <Link to="/keyboard-navigation">Keyboard Navigation</Link>
