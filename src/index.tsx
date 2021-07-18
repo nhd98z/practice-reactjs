58dd1adcd817576311fb2ff076da3786ceefa216
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2021-07-18 22:17:39 +0700

    fix: test

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import io from 'socket.io-client';
 
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
+
+if (SOCKET_URL === undefined) {
+  throw new Error('REACT_APP_SOCKET_URL not found.');
+}
+
 // Run project express-socketio-tutorial.
-export const socket = io('http://localhost:8080/', {
+export const socket = io(SOCKET_URL, {
   autoConnect: false,
 });
 
